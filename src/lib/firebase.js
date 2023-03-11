a45ffcf4fab7a2155242c8c141b4e2f33ed1260b
diodzi <diodzi@outlook.com>
2023-03-11 02:41:55 -0500

    applied prettier formatting (idk why it didn't before)

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
 import { writable } from 'svelte/store'
 
 export const firebaseConfig = {
-    apiKey: 'AIzaSyDwrzxnkzCCGu1RYFzMUuP2gR-qOIWgOk4',
-    authDomain: 'insecurities-3bdfb.firebaseapp.com',
-    projectId: 'insecurities-3bdfb',
-    storageBucket: 'insecurities-3bdfb.appspot.com',
-    messagingSenderId: '484524668724',
-    appId: '1:484524668724:web:3918796daec4f0ff329761',
+  apiKey: 'AIzaSyDwrzxnkzCCGu1RYFzMUuP2gR-qOIWgOk4',
+  authDomain: 'insecurities-3bdfb.firebaseapp.com',
+  projectId: 'insecurities-3bdfb',
+  storageBucket: 'insecurities-3bdfb.appspot.com',
+  messagingSenderId: '484524668724',
+  appId: '1:484524668724:web:3918796daec4f0ff329761',
 }
 
 export const app = initializeApp(firebaseConfig)
@@ -21,16 +21,16 @@ export const auth = getAuth(app)
 export const loggedIn = writable(false)
 
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-      loggedIn.set(true)
-      console.log('im in')
-    } else {
-      loggedIn.set(false)
-    }
-  })
-
-  export function logOut() {
-    signOut(auth).then(() => {
-      alert('logged out')
-    })
+  if (user) {
+    loggedIn.set(true)
+    console.log('im in')
+  } else {
+    loggedIn.set(false)
   }
+})
+
+export function logOut() {
+  signOut(auth).then(() => {
+    alert('logged out')
+  })
+}
